0724a2b706e4686239e56f82c5ba75d447bcf39e
Kamal El Alami <kelalami123@gmail.com>
2025-01-12 22:57:02 +0100

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,23 @@
+// simple function
+
+fn simple_function()
+{
+    println!("i m just a simple function 💅🎀");
+}
+// function with return value
+
+fn addition(a:i32, b:i32) -> i32
+{
+    return (a + b);
+}
+
+// function can return value without using return keyword
+
+fn division(a:i32, b:i32) -> i32
+{
+    a / b
+}
+
+fn main() {
+    println!("Hello, world!");
+}
